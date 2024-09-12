80e8a6b276ad73370c71da13fdb691de3709213f
Rill <jakueenak@gmail.com>
2024-09-12 12:58:50 +0700

    Refactor: rest/shorturl request and response into 1 file

--- internal/adapters/handlers/rest/shorturl.go
+++ internal/adapters/handlers/rest/shorturl.go
@@ -5,3 +5,13 @@ type CreateShortURLRequest struct {
 	CustomURL   *string `json:"custom_url,omitempty" validate:"omitempty,alphanum,gte=5,lt=20" example:"shorturl"`
 	Duration    *int8   `json:"duration,omitempty" validate:"omitempty,gte=1,lt=90" example:"3"`
 }
+
+type createShortURLResponse struct {
+	URL      string `json:"url"`
+	ShortURL string `json:"short_url"`
+	Expires  string `json:"expires"`
+}
+
+type ShortURLResponseSuccess struct {
+	Data createShortURLResponse `json:"data"`
+}
